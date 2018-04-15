5516723296c64395d32d2541b93ab00fb6104852
eternalamenity <33001623+eternalamenity@users.noreply.github.com>
2018-04-15 14:37:59 +0200

    GraphicTestImplementation

--- BluetoothAPP/MainActivity.cs
+++ BluetoothAPP/MainActivity.cs
@@ -4,15 +4,14 @@ using Android.OS;
 
 namespace BluetoothAPP
 {
-    [Activity(Label = "BluetoothAPP", MainLauncher = true)]
+    [Activity(Label = "BluetoothAPP", MainLauncher = true, Icon = "@drawable/Lock")]
     public class MainActivity : Activity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             // Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.Main);
-            //test1
+            SetContentView(Resource.Layout.MainActivity);
         }
     }
 }
