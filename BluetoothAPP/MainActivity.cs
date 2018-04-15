c7e9b1cd5e2a24d7106bb00609ae0dbeb3754141
eternalamenity <33001623+eternalamenity@users.noreply.github.com>
2018-04-15 13:16:55 +0200

    testing push pt1

--- BluetoothAPP/MainActivity.cs
+++ BluetoothAPP/MainActivity.cs
@@ -10,9 +10,9 @@ namespace BluetoothAPP
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-            //SomeComment
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
+            //test1
         }
     }
 }
