0822b450b0a85251290150cccd709268a45e3228
eternalamenity <33001623+eternalamenity@users.noreply.github.com>
2018-04-23 15:17:20 +0200

    separate_class_for_bluetoothManaging

--- BluetoothAPP/Fragments/UsersFragment.cs
+++ BluetoothAPP/Fragments/UsersFragment.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using Android.App;
-using Android.Bluetooth;
 using Android.Content;
 using Android.OS;
 using Android.Runtime;
@@ -12,6 +11,7 @@ using Android.Util;
 using Android.Views;
 using Android.Widget;
 using BluetoothAPP.Activities;
+using BluetoothAPP.Model;
 using Java.Util;
 
 namespace BluetoothAPP.Fragments
@@ -19,13 +19,12 @@ namespace BluetoothAPP.Fragments
     public class UsersFragment : Fragment
     {
         Activity activ = new UsersActivity();
-        BluetoothDevice device;
-        BluetoothAdapter adapter;
-        BluetoothSocket socket;
+        BluetoothManage bluetooth = new BluetoothManage();
+
         Button btnConnect;
         Button record;
         TextView txtStatus;
-
+        
         public override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -39,78 +38,21 @@ namespace BluetoothAPP.Fragments
             record = mainView.FindViewById<Button>(Resource.Id.record);
             btnConnect = mainView.FindViewById<Button>(Resource.Id.connectBtn);
             txtStatus = mainView.FindViewById<TextView>(Resource.Id.txtStatus);
-           
+
             //Event-Click handlers
-            record.Click += BtnRecord_Click;
-            btnConnect.Click += BtnConnect_Click;
+            record.Click += (s, e) =>
+            {
+                bluetooth.BtnRecord_Click(s, e);
+            };
+            btnConnect.Click += (s, e) =>
+            {
+                bluetooth.BtnConnect_Click(s, e, btnConnect, record, txtStatus);
+            };
 
             //Enabling/Disabling button to press
-            EnableOrDisableAllMainFunctions(false);
+            bluetooth.EnableOrDisableAllMainFunctions(false, record);
 
             return mainView;
-        }
-
-
-
-        private void EnableOrDisableAllMainFunctions(bool isEnabled)
-        {
-            record.Enabled = isEnabled;
-        }
-
-        async void BtnConnect_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                //Using default adapter of a phone
-                adapter = BluetoothAdapter.DefaultAdapter;
-                if (adapter == null)
-                    throw new Exception("No Bluetooth adapter found.");
-
-                if (!adapter.IsEnabled)
-                    throw new Exception("Bluetooth adapter is not enabled.");
-
-                //Looking for HC-05 bluetooth adapter
-                device = (from bd in adapter.BondedDevices
-                          where bd.Name == "HC-05"
-                          select bd).FirstOrDefault();
-
-                if (device == null)
-                    throw new Exception("STM32 device not found.");
-
-                socket = device.CreateRfcommSocketToServiceRecord(UUID.FromString("00001101-0000-1000-8000-00805f9b34fb"));
-                await socket.ConnectAsync();
-                //Shows text on screen of app when STM connected
-                txtStatus.Text = "STM Connected";
-                EnableOrDisableAllMainFunctions(true);
-                btnConnect.Enabled = false;
-            }
-            catch (Exception ex)
-            {
-                txtStatus.Text = ex.Message;
-                Toast.MakeText(activ, ex.Message, ToastLength.Short);
-            }
-        }
-
-
-        void BtnRecord_Click(object sender, EventArgs e)
-        {
-            //Sending 1 through bluetooth by Record Button
-            RobotMainFunction("1");
-        }
-
-        
-        private async void RobotMainFunction(string strDirection)
-        {
-            try
-            {
-                //Procedure that sends byte through socket
-                byte[] buffer = Encoding.ASCII.GetBytes(strDirection);
-                await socket.OutputStream.WriteAsync(buffer, 0, buffer.Length);
-            }
-            catch (Exception ex)
-            {
-                Toast.MakeText(activ, ex.Message, ToastLength.Short);
-            }
         }
     }
 }
---
+++ BluetoothAPP/Model/BluetoothManage.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Android.App;
+using Android.Bluetooth;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Java.Util;
+
+namespace BluetoothAPP.Model
+{
+    public class BluetoothManage
+    {
+        Activity activ = null;
+        BluetoothDevice device;
+        BluetoothAdapter adapter;
+        BluetoothSocket socket;
+
+
+        public void EnableOrDisableAllMainFunctions(bool isEnabled, Button record)
+        {
+            record.Enabled = isEnabled;
+        }
+
+        public async void BtnConnect_Click(object sender, EventArgs e, Button btnConnect, Button record, TextView txtStatus)
+        {
+            try
+            {
+                //Using default adapter of a phone
+                adapter = BluetoothAdapter.DefaultAdapter;
+                if (adapter == null)
+                    throw new Exception("No Bluetooth adapter found.");
+
+                if (!adapter.IsEnabled)
+                    throw new Exception("Bluetooth adapter is not enabled.");
+
+                //Looking for HC-05 bluetooth adapter
+                device = (from bd in adapter.BondedDevices
+                          where bd.Name == "HC-05"
+                          select bd).FirstOrDefault();
+
+                if (device == null)
+                    throw new Exception("STM32 device not found.");
+
+                socket = device.CreateRfcommSocketToServiceRecord(UUID.FromString("00001101-0000-1000-8000-00805f9b34fb"));
+                await socket.ConnectAsync();
+                //Shows text on screen of app when STM connected
+                txtStatus.Text = "STM Connected";
+                EnableOrDisableAllMainFunctions(true, record);
+                btnConnect.Enabled = false;
+            }
+            catch (Exception ex)
+            {
+                txtStatus.Text = ex.Message;
+                Toast.MakeText(activ, ex.Message, ToastLength.Short);
+            }
+        }
+
+
+        public void BtnRecord_Click(object sender, EventArgs e)
+        {
+            //Sending 1 through bluetooth by Record Button
+            RobotMainFunction("1");
+        }
+
+
+        private async void RobotMainFunction(string strDirection)
+        {
+            try
+            {
+                //Procedure that sends byte through socket
+                byte[] buffer = Encoding.ASCII.GetBytes(strDirection);
+                await socket.OutputStream.WriteAsync(buffer, 0, buffer.Length);
+            }
+            catch (Exception ex)
+            {
+                Toast.MakeText(activ, ex.Message, ToastLength.Short);
+            }
+        }
+    }
+}
