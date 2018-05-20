da232b6d6e4967ad589a691230525921a23a99cc
kubiesh <jakub.z.dabrowski@student.put.poznan.pl>
2018-05-20 13:04:59 +0200

    database parameters initialization

---
+++ inc/database_parameters.h
@@ -0,0 +1,15 @@
+/*
+ * database_parameters.h
+ *
+ *  Created on: 20.05.2018
+ *      Author: HP 8560w
+ */
+
+#ifndef DATABASE_PARAMETERS_H_
+#define DATABASE_PARAMETERS_H_
+
+#define Database_NumberOfUsers 20
+#define Database_TupleSize 142
+
+
+#endif /* DATABASE_PARAMETERS_H_ */
