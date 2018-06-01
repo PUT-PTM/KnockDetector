58763041cfa0b2f408e32237e4e10c55512d00ae
kubiesh <jakub.z.dabrowski@student.put.poznan.pl>
2018-06-01 19:56:13 +0200

    Main controller clean and update

--- inc/maincontroller.h
+++ inc/maincontroller.h
@@ -10,6 +10,13 @@
 
 #include "sensor.h"
 #include "sdmodule.h"
+#include "bluetooth_controller.h"
+#include "database_controller.h"
 
+#define DIODES_ONBOARD_PINS  GPIO_Pin_12 | GPIO_Pin_13| GPIO_Pin_14| GPIO_Pin_15
+#define DIODES_ONBOARD_PORT GPIOD
+
+void Controller_Configuration(void);
+void DiodesOnBoard_Config(void);
 
 #endif /* MAINCONTROLLER_H_ */
--- src/maincontroller.c
+++ src/maincontroller.c
@@ -12,44 +12,36 @@
 #include "stm32f4_discovery.h"
 #include "maincontroller.h"
 
-#define DIODES_ON_BOARD  GPIO_Pin_12 | GPIO_Pin_13| GPIO_Pin_14| GPIO_Pin_15
 
-char* filename = "testtt.txt";
-char* buffer= "123456789";
+
+
+
+void Zaswiec(void) {
+	GPIO_SetBits(GPIOD, DIODES_ONBOARD_PINS);
+}
+
+int main(void) {
+	Controller_Configuration();
+
+	for (;;)
+		;
+}
+
+void Controller_Configuration(void) {
+	DiodesOnBoard_Config();
+	Bluetooth_Configuration();
+	Database_Configuration();
+}
 
 void DiodesOnBoard_Config(void) {
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
 	GPIO_InitTypeDef GPIO_InitStructure;
 	//DIODY
 	/* Configure PD12, PD13, PD14 and PD15 in output pushpull mode */
-	GPIO_InitStructure.GPIO_Pin = DIODES_ON_BOARD;
+	GPIO_InitStructure.GPIO_Pin = DIODES_ONBOARD_PINS;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
 	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
 	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
-	GPIO_Init(GPIOD, &GPIO_InitStructure);
-}
-
-void Zaswiec(void) {
-
-	GPIO_SetBits(GPIOD, DIODES_ON_BOARD);
-}
-
-int main(void) {
-	/* Only for tests */
-	Sensor_Configuration();
-	DiodesOnBoard_Config();
-	SDmodule_Configuration();
-	UINT loadedBytes;
-	char dane[3000];
-	SDmodule_ReadFile(filename, &dane, &loadedBytes);
-	/*if (c2[0]=='1') {
-	Zaswiec();
-	}*/
-	for (;;)
-		;
-}
-
-void Controller_Configuration(void) {
-
+	GPIO_Init(DIODES_ONBOARD_PORT, &GPIO_InitStructure);
 }
