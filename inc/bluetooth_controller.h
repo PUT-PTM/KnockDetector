288ba2e08923d6047e9e30820d76f58734e6d747
kubiesh <jakub.z.dabrowski@student.put.poznan.pl>
2018-06-01 19:25:05 +0200

    Bluetooth controller cleaning and refactoring

--- inc/bluetooth_controller.h
+++ inc/bluetooth_controller.h
@@ -8,10 +8,9 @@
 #ifndef BLUETOOTH_CONTROLLER_H_
 #define BLUETOOTH_CONTROLLER_H_
 
-void Bluetooth_Init(void);
-void Bluetooth_NVIC_Configuration(void);
-void Bluetooth_GPIO_Configuration(void);
-void Bluetooth_USART_Configuration(void);
+#include "stm32f4xx.h"
+
+void Bluetooth_Configuration(void);
 void USART3_IRQHandler(void);
 //void UARTSend(const unsigned char *pucBuffer, unsigned long ulCount);
 void Bluetooth_Send(char[], unsigned long);
--- src/bluetooth_controller.c
+++ src/bluetooth_controller.c
@@ -5,11 +5,15 @@
  *      Author: Przemek
  */
 
-#include "stm32f4xx.h"
-#include "stm32f4_discovery.h"
+
 #include "bluetooth_controller.h"
 
 static int received;
+
+static void Bluetooth_GPIO_Configuration(void);
+static void Bluetooth_USART_Configuration(void);
+static void Bluetooth_NVIC_Configuration(void);
+
 
 void USART3_IRQHandler(void) {
 	if ((USART3->SR & USART_FLAG_RXNE) != (u16) RESET) {
@@ -29,39 +33,17 @@ void USART3_IRQHandler(void) {
 	}
 }
 
-void Bluetooth_Init(void) {
-	const unsigned char welcome_str[] = " Welcome to Bluetooth!\r\n";
-
-	// wlaczenie taktowania wybranego portu
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
-	// zegar dla portu GPIO z którego wykorzystany zostanie pin jako wejœcie ADC (PA1)
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
-	// wlaczenie taktowania wybranego uk³adu USART
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART3, ENABLE);
-
-	/* NVIC Configuration */
+void Bluetooth_Configuration(void) {
 	Bluetooth_NVIC_Configuration();
-
-	/* Configure the GPIOs */
 	Bluetooth_GPIO_Configuration();
-
-	/* Configure the USART1 */
 	Bluetooth_USART_Configuration();
-
-	/* Enable the USART1 Receive interrupt: this interrupt is generated when the
-	 USART1 receive data register is not empty */
-	USART_ITConfig(USART3, USART_IT_RXNE, ENABLE);
-
 	/* print welcome information */
 	//UARTSend(welcome_str, sizeof(welcome_str));
 }
 
-/*******************************************************************************
- * Function Name  : GPIO_Configuration
- * Description    : Configures the different GPIO ports
- *******************************************************************************/
-void Bluetooth_GPIO_Configuration(void) {
+static void Bluetooth_GPIO_Configuration(void) {
+	// wlaczenie taktowania wybranego portu
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
 
 	GPIO_InitTypeDef GPIO_InitStructure;
 	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10 | GPIO_Pin_11;
@@ -75,27 +57,11 @@ void Bluetooth_GPIO_Configuration(void) {
 	GPIO_PinAFConfig(GPIOC, GPIO_PinSource10, GPIO_AF_USART3);
 	GPIO_PinAFConfig(GPIOC, GPIO_PinSource11, GPIO_AF_USART3);
 
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14
-			| GPIO_Pin_15;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
-	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
-	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
-	GPIO_Init(GPIOD, &GPIO_InitStructure);
-
-	//inicjalizacja wejœcia ADC
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AN;
-	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
-	GPIO_Init(GPIOA, &GPIO_InitStructure);
-
 }
 
-/*******************************************************************************
- * Function Name  : USART_Configuration
- * Description    : Configures the USART1
- *******************************************************************************/
-void Bluetooth_USART_Configuration(void) {
+static void Bluetooth_USART_Configuration(void) {
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART3, ENABLE);
+
 	USART_InitTypeDef USART_InitStructure;
 
 	/* USART1 configuration ------------------------------------------------------*/
@@ -113,7 +79,7 @@ void Bluetooth_USART_Configuration(void) {
 	USART_Cmd(USART3, ENABLE);
 }
 
-void Bluetooth_NVIC_Configuration(void) {
+static void Bluetooth_NVIC_Configuration(void) {
 	NVIC_InitTypeDef NVIC_InitStructure;
 
 	/* Enable the USARTx Interrupt */
@@ -122,6 +88,9 @@ void Bluetooth_NVIC_Configuration(void) {
 	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
 	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
 	NVIC_Init(&NVIC_InitStructure);
+	/* Enable the USART3 Receive interrupt: this interrupt is generated when the
+	 USART3 receive data register is not empty */
+	USART_ITConfig(USART3, USART_IT_RXNE, ENABLE);
 }
 
 
