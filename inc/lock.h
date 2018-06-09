83a5271ad94a058975d8a25cf14bfec8d53527df
przemyslawczajka <przemyslawczajka@users.noreply.github.com>
2018-06-09 18:30:15 +0200

    create lock

---
+++ inc/lock.h
@@ -0,0 +1,16 @@
+/*
+ * lock.h
+ *
+ *  Created on: 09.06.2018
+ *      Author: Przemek
+ */
+
+#ifndef LOCK_H_
+#define LOCK_H_
+
+#include "stm32f4xx.h"
+#include "stm32f4_discovery.h"
+
+void lock_init(void);
+
+#endif /* LOCK_H_ */
