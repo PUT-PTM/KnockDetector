6f13814dfc9be616a1fdc816b01cf83324611832
kubiesh <jakub.z.dabrowski@student.put.poznan.pl>
2018-06-09 13:42:08 +0200

    SD read/write fix

--- src/sdmodule.c
+++ src/sdmodule.c
@@ -27,40 +27,35 @@ void SDmodule_Configuration(void) {
 	fpu_enable();
 	delay_init(168);
 	SPI_SD_Init();
+	f_mount(0, &fatfs);
 }
 
 uint8_t SDmodule_ReadFile(char* file_name, char * buffer, UINT * loaded_bytes) {
 	int file_size = 0;
 	UINT loaded_bytes_nonptr=0; /* For copy to loaded_bytes */
 
-	fresult = f_mount(0, &fatfs);
+	fresult = f_open(&file, file_name, FA_OPEN_ALWAYS | FA_READ);
 
 	if (fresult == FR_OK) {
-		fresult = f_open(&file, file_name, FA_OPEN_ALWAYS | FA_READ);
+		file_size = f_size(&file);
+		fresult = f_read(&file, &buffer[0], file_size, &loaded_bytes_nonptr);
+		*loaded_bytes=loaded_bytes_nonptr;
 
-		if (fresult == FR_OK) {
-			file_size = f_size(&file);
-			fresult = f_read(&file, &buffer[0], file_size, &loaded_bytes_nonptr);
-			*loaded_bytes=loaded_bytes_nonptr;
-			fresult = f_close(&file);
-		}
 	}
+	f_close(&file);
 	return fresult;
 }
 
 uint8_t SDmodule_WriteFile(char * file_name, char * file_content, UINT file_content_size) {
 	unsigned int storedbytes;
-	fresult = f_mount(0, &fatfs);
-
+	fresult = f_open(&file, file_name, FA_OPEN_ALWAYS | FA_WRITE);
 	if (fresult == FR_OK) {
-		f_unlink(file_name);
-		fresult = f_open(&file, file_name, FA_OPEN_ALWAYS | FA_WRITE);
+		fresult = f_write(&file, file_content, file_content_size, &storedbytes);
 
-		if (fresult == FR_OK) {
-			fresult = f_write(&file, file_content, file_content_size, &storedbytes);
-			fresult = f_close(&file);
-		}
+		int b = f_truncate(&file);
+
 	}
-
+	f_close(&file);
+	int a = fresult;
 	return fresult;
 }
