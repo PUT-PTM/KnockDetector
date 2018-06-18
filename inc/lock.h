8efcc69d8ec8605cb4c4dd5225a9c4a8f7c36321
przemyslawczajka <przemyslawczajka@users.noreply.github.com>
2018-06-18 14:40:35 +0200

    Add lock to master

---
+++ inc/lock.h
@@ -0,0 +1,23 @@
+/*
+ * lock.h
+ *
+ *  Created on: 09.06.2018
+ *      Author: Przemek
+ */
+
+#ifndef LOCK_H_
+#define LOCK_H_
+
+#include "stm32f4xx.h"
+#include "bool.h"
+
+#define Engine_IN1 GPIO_Pin_1
+#define Engine_IN2 GPIO_Pin_2
+#define Engine_Port GPIOA
+#define Engine_RCC RCC_AHB1Periph_GPIOA
+
+void Lock_Configuration(void);
+void Lock_Unlock(void);
+void Lock_Clock(void);
+
+#endif /* LOCK_H_ */
---
+++ src/lock.c
@@ -0,0 +1,47 @@
+/*
+ * lock.c
+ *
+ *  Created on: 09.06.2018
+ *      Author: Przemek
+ */
+
+#include "lock.h"
+
+bool Lock_state;
+
+static void Lock_TimerConfig(void);
+
+void Lock_Configuration(void) {
+	RCC_AHB1PeriphClockCmd(Engine_RCC, ENABLE);
+
+	GPIO_InitTypeDef GPIO_InitStructure;
+	GPIO_InitStructure.GPIO_Pin = Engine_IN1 | Engine_IN2;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	GPIO_Init(Engine_Port, &GPIO_InitStructure);
+}
+
+static
+void Lock_TimerConfig(void) {
+
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM4, ENABLE);
+
+	TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
+
+	TIM_TimeBaseStructure.TIM_Period = 8399;
+	TIM_TimeBaseStructure.TIM_Prescaler = 9999;
+	TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV1;
+	TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
+	TIM_TimeBaseInit(TIM4, &TIM_TimeBaseStructure);
+	TIM_Cmd(TIM4, ENABLE);
+}
+
+void Lock_Unlock(void) {
+
+}
+
+void Lock_Lock(void) {
+
+}
