858db83fdfa30a48d3dfd9c5e274251c6ff8464f
przemyslawczajka <przemyslawczajka@users.noreply.github.com>
2018-06-18 14:36:14 +0200

    Use defines

--- src/lock.c
+++ src/lock.c
@@ -7,18 +7,20 @@
 
 #include "lock.h"
 
+bool Lock_state;
+
 static void Lock_TimerConfig(void);
 
 void Lock_Configuration(void) {
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+	RCC_AHB1PeriphClockCmd(Engine_RCC, ENABLE);
 
 	GPIO_InitTypeDef GPIO_InitStructure;
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_1 | GPIO_Pin_2;
+	GPIO_InitStructure.GPIO_Pin = Engine_IN1 | Engine_IN2;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
 	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
 	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
-	GPIO_Init(GPIOA, &GPIO_InitStructure);
+	GPIO_Init(Engine_Port, &GPIO_InitStructure);
 }
 
 static
